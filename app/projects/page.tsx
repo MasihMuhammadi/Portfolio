ed1f1a65cfe419572209b53d2f18c260d5e2a79b
MasihMuhammadi <masihmuhammadi202@gmail.com>
2024-11-20 01:03:42 +0430

    fix responsive issues

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -20,62 +20,49 @@ const galleryItems: GalleryItem[] = [
   {
     id: 1,
     imageSrc: reserv,
-    title: "Project 1",
-    description: "Description for project 1",
+    title: "Reserv Best",
+    description: "Full Stact Project for reserving Wedding hotels in Kabul",
     link: "https://reserv.vercel.app",
   },
   {
     id: 2,
     imageSrc: buildResume,
-    title: "Project 2",
-    description: "Description for project 2",
+    title: "Build Resume",
+    description: "Project for creating dynamic Resume use templates",
     link: "https://buildResume.vercel.app",
   },
   {
     id: 3,
     imageSrc: checkIn,
-    title: "Project 3",
-    description: "Description for project 3",
+    title: "Automated attendance",
+    description: "School Management system, with automated attendance feature ",
     link: "https://check-in-now.vercel.app",
   },
   {
     id: 4,
     imageSrc: squadGame,
-    title: "Project 4",
-    description: "Description for project 4",
+    title: "Squad Game",
+    description: "A basic brain game",
     link: "https://squadGame.vercel.app",
   },
   {
     id: 5,
     imageSrc: Weather,
-    title: "Project 5",
-    description: "Description for project 5",
+    title: "Weather App",
+    description: "Weather App developed with Vue.js",
     link: "https://masihweather.vercel.app",
   },
   {
     id: 6,
     imageSrc: memory,
-    title: "Project 6",
-    description: "Description for project 6",
+    title: "Memory Card Game",
+    description: "Simple Memory Game, find 2 same cards",
     link: "https://masih-memory.vercel.app",
   },
 ];
 
 const MyProjects: React.FC = () => {
   const [activeProject, setActiveProject] = useState<number | null>(0);
-  const [screenSize, setScreenSize] = useState<number>(window.innerWidth);
-
-  // Update screen size on resize
-  useEffect(() => {
-    const handleResize = () => {
-      setScreenSize(window.innerWidth);
-    };
-    window.addEventListener("resize", handleResize);
-
-    return () => {
-      window.removeEventListener("resize", handleResize);
-    };
-  });
 
   return (
     <div className="overflow-hidden py-10">
@@ -86,75 +73,70 @@ const MyProjects: React.FC = () => {
       >
         See All Projects
       </a>
-
-      {screenSize > 450 ? (
-        // Larger screens
-        <div className="min-h-screen flex items-start mt-7 justify-start px-4 sm:px-8 md:px-16">
-          <div className="relative p-8 flex flex-wrap justify-center gap-8">
-            {galleryItems.map((item, index) => (
-              <div
-                key={item.id}
-                className={`absolute transition-all duration-500 ease-in-out w-full h-auto shadow-md shadow-yellow-500 ${
-                  activeProject === index ? "scale-110 z-20" : "scale-90 z-10"
-                }`}
-                style={{
-                  top: activeProject === index ? "10px" : `${index * 60}px`,
-                  left: activeProject === index ? "400px" : `${index * 20}px`,
-                  zIndex: activeProject === index ? 20 : index,
-                  width: activeProject === index ? "800px" : "300px",
-                }}
-                onClick={() =>
-                  setActiveProject((prev) => (prev === index ? null : index))
-                }
-              >
-                <div className="relative group overflow-hidden rounded-lg shadow-lg">
-                  <div className="relative">
-                    <Image
-                      src={item.imageSrc}
-                      alt={item.title}
-                      className="w-full h-full object-cover"
-                    />
-                  </div>
-                </div>
-                {activeProject === index && (
-                  <a
-                    href={item.link}
-                    className="absolute bottom-5 bg-yellow-500 text-white px-10 py-2 rounded-lg left-[45%]"
-                    target="_blank"
-                  >
-                    Visit
-                  </a>
-                )}
-              </div>
-            ))}
-          </div>
-        </div>
-      ) : (
-        // Smaller screens
-        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-10 px-4 sm:px-10 mt-5">
+      <div className="min-h-screen hidden md:flex items-start mt-7 justify-start px-4 sm:px-8 md:px-16">
+        <div className="relative p-8 flex flex-wrap justify-center gap-8">
           {galleryItems.map((item, index) => (
             <div
               key={item.id}
-              className="bg-black text-white border rounded-xl border-white shadow-sm shadow-yellow-500 p-3 mt-2 text-center flex flex-col items-center justify-center hover:scale-105 transition-transform duration-200"
+              className={`absolute transition-all duration-500 ease-in-out w-full h-auto shadow-md shadow-yellow-500 ${
+                activeProject === index ? "scale-110 z-20" : "scale-90 z-10"
+              }`}
+              style={{
+                top: activeProject === index ? "10px" : `${index * 60}px`,
+                left: activeProject === index ? "400px" : `${index * 20}px`,
+                zIndex: activeProject === index ? 20 : index,
+                width: activeProject === index ? "800px" : "300px",
+              }}
+              onClick={() =>
+                setActiveProject((prev) => (prev === index ? null : index))
+              }
             >
-              <Image
-                src={item.imageSrc}
-                width={400}
-                height={200}
-                alt={item.title}
-              />
-              <p className="text-yellow-600 mt-3">{item.title}</p>
-              <a
-                className="bg-yellow-500 p-2 px-4 rounded text-black mt-4 hover:scale-95"
-                href={item.link}
-                target="_blank"
-              >
-                Visit
-              </a>
+              <div className="relative group overflow-hidden rounded-lg shadow-lg">
+                <div className="relative">
+                  <Image
+                    src={item.imageSrc}
+                    alt={item.title}
+                    className="w-full h-full object-cover"
+                  />
+                </div>
+              </div>
+              {activeProject === index && (
+                <a
+                  href={item.link}
+                  className="absolute bottom-5 bg-yellow-500 text-white px-10 py-2 rounded-lg left-[45%]"
+                  target="_blank"
+                >
+                  Visit
+                </a>
+              )}
             </div>
           ))}
         </div>
-      )}
+      </div>
+
+      <div className="sm:grid-cols-1 md:grid-cols-2 lg:hidden  gap-10 px-4 sm:px-10 mt-5">
+        {galleryItems.map((item, index) => (
+          <div
+            key={item.id}
+            className="bg-black text-white border rounded-xl border-white shadow-sm shadow-yellow-500 p-3 mt-2 text-center flex flex-col items-center justify-center hover:scale-105 transition-transform duration-200"
+          >
+            <Image
+              src={item.imageSrc}
+              width={400}
+              height={200}
+              alt={item.title}
+            />
+            <p className="text-yellow-600 mt-3">{item.title}</p>
+            <a
+              className="bg-yellow-500 p-2 px-4 rounded text-black mt-4 hover:scale-95"
+              href={item.link}
+              target="_blank"
+            >
+              Visit
+            </a>
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
